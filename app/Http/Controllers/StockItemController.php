63e3e1edda21d8f669ee9652bbf074f43e838c96
Mikhail A <mg@abramishvili.net>
2022-02-14 17:08:37 +0500

    .

--- app/Http/Controllers/StockItemController.php
+++ app/Http/Controllers/StockItemController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 
 class StockItemController extends Controller
 {
+    public function item($id)
+    {
+        return StockItem::with('balamces')->find($id);
+    }
+
     public function itemsToBuy()
     {
         return StockItem::where('amount', '<', 0)->get();
