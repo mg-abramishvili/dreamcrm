743b4379686e67a2fc07d31067656214914f28be
Mikhail A <mg@abramishvili.net>
2022-02-18 18:51:31 +0500

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -39,7 +39,7 @@ Vue.filter('date', function (value) {
 })
 Vue.filter('time', function (value) {
     if (!value) return ''
-    return moment.tz(value, 'Europe/Moscow').format('H:mm')
+    return moment.utc(value).format('H:mm')
 })
 Vue.filter('formatDateShort', function (value) {
     if (!value) return ''
