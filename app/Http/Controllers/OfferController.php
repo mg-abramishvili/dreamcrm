660ae72983b0ff82993636800fbf101438a596ff
Mikhail A <mg@abramishvili.net>
2022-03-01 17:27:55 +0500

    .

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -44,7 +44,7 @@ class OfferController extends Controller
     {
         $offer = Offer::with('calculations.boxes', 'calculations.catalogItems')->find($id);
 
-        $pdf = PDF::setOptions(['tempDir' => storage_path('fonts/')])->loadView('offers.pdf', compact('offer'));
+        $pdf = PDF::loadView('offers.pdf', compact('offer'));
         // unlink(public_path() . '/uploads/offers/' . 'kp_' . $offer->id . '.pdf');
         $pdf->save(public_path() . '/uploads/offers/' . 'kp_' . $offer->id . '.pdf');
     }
