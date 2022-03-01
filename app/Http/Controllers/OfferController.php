1f6975dae2ec7666499b8e9d445e33fd3c6f49f9
Mikhail A <mg@abramishvili.net>
2022-03-01 17:36:08 +0500

    .

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -44,7 +44,7 @@ class OfferController extends Controller
     {
         $offer = Offer::with('calculations.boxes', 'calculations.catalogItems')->find($id);
 
-        PDF::setOptions(['tempDir', public_path() . '/uploads'])->loadView('offers.pdf', compact('offer'))->save(public_path() . '/uploads/offers/' . 'kp_' . $offer->id . '.pdf');
+        PDF::setOptions(['tempDir', public_path() . '/uploads'])->loadView('offers.pdf', compact('offer'))->save('/uploads/offers/' . 'kp_' . $offer->id . '.pdf');
     }
 
     // public function offerPDF($id)
