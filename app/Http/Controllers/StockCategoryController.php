47fed5375a08f5482d877d646a3c876a3f5e9ba5
Mikhail A <mg@abramishvili.net>
2022-03-14 20:44:37 +0500

    .

--- app/Http/Controllers/StockCategoryController.php
+++ app/Http/Controllers/StockCategoryController.php
@@ -16,7 +16,7 @@ class StockCategoryController extends Controller
     {
         return StockCategory::with(
             [
-                'items' => function ($q) { $q->with('balances')->orderBy('name', SORT_NATURAL); }
+                'items' => function ($q) { $q->with('balances')->orderByRaw('name::int'); }
             ]
         )
         ->find($id);
