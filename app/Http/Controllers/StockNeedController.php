5e3d9c5ec61fab09b3c903d5dc1540f23131e6dd
Mikhail A <mg@abramishvili.net>
2022-06-14 19:05:43 +0500

    .

--- app/Http/Controllers/StockNeedController.php
+++ app/Http/Controllers/StockNeedController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\StockNeed;
-use App\Http\Resources\StockNeedsResource;
+use App\Http\Resources\stock\StockNeedsResource;
 use Illuminate\Http\Request;
 
 class StockNeedController extends Controller
