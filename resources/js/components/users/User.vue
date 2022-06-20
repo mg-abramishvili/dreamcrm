951d94b78f1416f3bb19a82148f753b8dad73880
Mikhail A <mg@abramishvili.net>
2022-06-20 17:24:04 +0500

    .

--- resources/js/components/users/User.vue
+++ resources/js/components/users/User.vue
@@ -66,7 +66,7 @@
                 <p class="d-flex align-items-center my-2">
                     <strong class="me-2">Макс. скидка:</strong>
                     <span>{{ user.max_discount }}%</span>
-                    <small @click="changePanel('discount')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 0.5;">
+                    <small v-if="$parent.$parent.user.id === 1" @click="changePanel('discount')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 0.5;">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block feather feather-edit align-middle me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
                     </small>
                     <ChangeDiscount v-if="views.changePanel == 'discount'" :user="user" />
