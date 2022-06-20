c3e4de484aeb4d672b110f50432490d4d81856aa
Mikhail A <mg@abramishvili.net>
2022-06-20 17:33:30 +0500

    .

--- resources/js/components/Sidebar.vue
+++ resources/js/components/Sidebar.vue
@@ -59,12 +59,12 @@
                         <span class="align-middle">Задачи</span>
                     </router-link>
                 </li>
-                <!-- <li v-if="this.$parent.user.permissions && this.$parent.user.permissions.can_see_users" class="sidebar-item">
+                <li v-if="this.$parent.user.id === 2" class="sidebar-item">
                     <router-link :to="{name: 'Users'}" class="sidebar-link">
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-users align-middle"><path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="9" cy="7" r="4"></circle><path d="M23 21v-2a4 4 0 0 0-3-3.87"></path><path d="M16 3.13a4 4 0 0 1 0 7.75"></path></svg>
-                        <span class="align-middle">Пользователи</span>
+                        <span class="align-middle">Юзеры</span>
                     </router-link>
-                </li> -->
+                </li>
             </ul>
 
         </div>
