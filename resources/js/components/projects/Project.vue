d300522277366585015f3286eb78568918b974de
Mikhail Abramishvili <mg@abramishvili.net>
2022-07-22 20:12:23 +0500

    .

--- resources/js/components/projects/Project.vue
+++ resources/js/components/projects/Project.vue
@@ -38,43 +38,49 @@
                 <div class="tab-pane" :class="{'active': selected.tab == 'general'}" role="tabpanel">
                     <div class="row">
                         <div class="col-12 col-lg-6">
-                            <p class="d-flex align-items-center">
-                                <strong class="me-2">Клиент:</strong>
+                            <div class="row align-items-center">
+                                <div class="col-4 text-end">
+                                    <strong class="me-2">Клиент:</strong>
+                                </div>
+                                <div class="col-8">
+                                    <span>
+                                        <template v-if="project.client">
+                                            {{ project.client.name }} (ИНН {{ project.client.inn }})
+                                        </template>
+                                        <template v-if="!project.client">
+                                            &mdash;
+                                        </template>
+                                    </span>
 
-                                <span>
-                                    <template v-if="project.client">
-                                        {{ project.client.name }}
-                                    </template>
-                                    <template v-if="!project.client">
-                                        &mdash;
-                                    </template>
-                                </span>
+                                    <small @click="changePanel('client')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 50%;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block feather feather-edit align-middle me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                                    </small>
 
-                                <small @click="changePanel('client')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 50%;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block feather feather-edit align-middle me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-                                </small>
+                                    <ChangeClient v-if="views.changePanel == 'client'" :project="project" />
+                                </div>
+                            </div>
 
-                                <ChangeClient v-if="views.changePanel == 'client'" :project="project" />
-                            </p>
+                            <div class="row align-items-center">
+                                <div class="col-4 text-end">
+                                    <strong class="me-2">Конечник:</strong>
+                                </div>
+                                <div class="col-8">
+                                    <span>
+                                        <template v-if="project.endclient">
+                                            {{ project.endclient.name }} (ИНН {{ project.endclient.inn }})
+                                        </template>
+                                        <template v-if="!project.endclient">
+                                            &mdash;
+                                        </template>
+                                    </span>
 
-                            <p class="d-flex align-items-center">
-                                <strong class="me-2">Конечник:</strong>
+                                    <small @click="changePanel('endClient')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 50%;">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block feather feather-edit align-middle me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
+                                    </small>
 
-                                <span>
-                                    <template v-if="project.endclient">
-                                        {{ project.endclient.name }}
-                                    </template>
-                                    <template v-if="!project.endclient">
-                                        &mdash;
-                                    </template>
-                                </span>
-
-                                <small @click="changePanel('endClient')" class="cursor-pointer text-muted fw-normal ms-2" style="opacity: 50%;">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="d-block feather feather-edit align-middle me-2"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
-                                </small>
-
-                                <ChangeEndClient v-if="views.changePanel == 'endClient'" :project="project" />
-                            </p>
+                                    <ChangeEndClient v-if="views.changePanel == 'endClient'" :project="project" />
+                                </div>
+                            </div>
 
                             <p class="d-flex align-items-center">
                                 <strong class="me-2">Ответственный:</strong>
